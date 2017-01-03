4168df878d8b7b173a984f206e3595082d0e6c6b
Farm <lucas.carotta@outlook.com>
2017-01-02 22:06:25 -0200

    Adding problem 3

--- project_euler/problem_3.rb
+++ project_euler/problem_3.rb
@@ -1,3 +1,10 @@
+=begin
+                            Largest prime factor
+
+    The prime factors of 13195 are 5, 7, 13 and 29.
+    What is the largest prime factor of the number 600851475143 ?
+=end
+
 def largest_prime( max )
     largest = 0, i = 2
 

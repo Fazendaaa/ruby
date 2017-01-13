be3c4aaa9596e951b69e91dc23bc448b7710b140
Farm <lucas.carotta@outlook.com>
2017-01-13 12:42:50 -0200

    Just changing the problem description

--- project_euler/problem_16.rb
+++ project_euler/problem_16.rb
@@ -1,9 +1,9 @@
 =begin
                                 Power digit sum
 
-    2¹ = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+    2**15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
-    What is the sum of the digits of the number 21000?
+    What is the sum of the digits of the number 2**1000?
 =end
 
 def power_digit_sum( base, power )

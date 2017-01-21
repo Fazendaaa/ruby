e234d2ec2cb3f2f4d3162977845be638f90551a5
Farm <lucas.carotta@outlook.com>
2017-01-21 20:09:16 -0200

    Adding problem 22. Solved

---
+++ project_euler/problem_22.rb
@@ -0,0 +1,41 @@
+=begin
+                                Names scores
+
+    Using  problem_22.txt,  a  46K text file containing over five-thousand first
+    names,  begin  by  sorting  it into alphabetical order. Then working out the
+    alphabetical  value  for  each name, multiply this value by its alphabetical
+    position in the list to obtain a name score.
+
+    For  example,  when the list is sorted into alphabetical order, COLIN, which
+    is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN
+    would obtain a score of 938 × 53 = 49714.
+
+    What is the total of all the name scores in the file?
+
+    obs: articles that helped me out:
+        *   http://stackoverflow.com/a/5545284/7092954
+=end
+
+#!/usr/bin/ruby
+
+def score_name( name )
+    sum = 0
+    name.each_byte { | letter | sum += letter - 64 }
+    return sum
+end
+
+def names_scores( filename )
+    names = []
+
+    File.open( filename, "r" ) do | file |
+        file.each_line do | line |
+            names.concat( line.split( /[w+",]/ ).select { | e | "" != e } )
+        end
+    end
+
+    names.sort.each_with_index.
+               map { | name, index | score_name( name ) * ( index + 1 ) }
+end
+
+puts names_scores( "problem_22.txt" ).
+     inject { | result, element | result = result + element }

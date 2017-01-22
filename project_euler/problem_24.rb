d6316fe99d0e2dfe7da9b4109c6a18f723046d41
Farm <lucas.carotta@outlook.com>
2017-01-22 19:40:40 -0200

    Adding problem 24. Solved

--- project_euler/problem_24.rb
+++ project_euler/problem_24.rb
@@ -12,12 +12,8 @@
     5, 6, 7, 8 and 9?
 =end
 
-def lexicographic_permutations( numbers )
-    permutations = numbers.sort
-
-    
-
-    return permutations
+def lexicographic_permutations( pos, numbers )
+    permutations = numbers.permutation.to_a[ pos - 1 ].join
 end
 
-puts lexicographic_permutations( [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ] )[ 1_000_000 + 1 ]
+puts lexicographic_permutations( 1_000_000, [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ] )

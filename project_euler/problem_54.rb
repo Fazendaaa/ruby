c0e773e558a3adb62dee88d1112ceb607b4680f0
Farm <lucas.carotta@outlook.com>
2017-02-08 12:48:24 -0200

    [NOT SOLVED] Did it some of the functions so far.

--- project_euler/problem_54.rb
+++ project_euler/problem_54.rb
@@ -59,6 +59,7 @@
 		*	http://stackoverflow.com/a/13634446/7092954
 		*	https://boardgamegeek.com/wiki/page/Standard_Deck_Playing_Card_Games&redirectedfrom=standard_deck_playing_card_game#
 		*	http://stackoverflow.com/a/76046/7092954
+		*	http://stackoverflow.com/a/17463812/7092954
 =end
 
 require_relative 'project_euler'
@@ -68,13 +69,101 @@ require_relative 'project_euler'
 	H = Hearts
 	D = Diamonds
 	C = Clubs
-	J = Jack
-	Q = Queen
-	K = King
-	A = Ace
 =end
 
+def __card_value( card )
+	number = card.split( '' )[ 0 ]
+
+	if 'J' == number
+		number = 11
+	elsif 'Q' == number
+		number = 12
+	elsif 'K' == number
+		number = 13
+	elsif 'A' == number
+		number = 14
+	end
+
+	return number
+end
+
+def card_value( card )
+	number = card.split( '' )[ 0..-2 ].join.to_i
+	number = __card_value( card ) if 0 == number
+
+	return number
+end
+
+def card_suit( card )
+	return card.split( '' )[ -1 ]
+end
+
+def check_pair( n, hand )
+	return hand.find_all { | e | n == hand.count( e ) }.uniq
+end
+
+def is_high_card( hand )
+	return hand.map { | e | card_value( card ) }.max
+end
+
+def is_one_pair( hand )
+	return 1 == check_pair( 2, hand ).length ? true : false
+end
+
+def is_two_pair( hand )
+	return 2 == check_pair( 2, hand ).length ? true : false
+end
+
+def is_three_of_a_kind( hand )
+	return nil != hand.find { | e | 3 == hand.count( e ) } ? true : false
+end
+
+def is_straight( hand )
+	cards = hand.map { | e | card_value( e ) }.sort
+	return ( 0..cards.length-2 ).all? { | i | cards[ i+1 ] - 1 == cards[ i ] }
+end
+	
+def is_flush( hand )
+	return 1 == hand.map { | e | card_suit( e ) }.uniq.length ? true : false
+end
+
+=begin
+	* Full House: Three of a kind and a pair.
+	* Four of a Kind: Four cards of the same value.
+	* Straight Flush: All cards are consecutive values of same suit.
+	* Royal Flush: Ten, Jack, Queen, King, Ace, in same suit.
+=end
+
+def is_full_house( hand )
+	return is_three_of_a_kind( hand ) && ( is_one_pair( hand ) || is_two_pair( hand ) )
+end
+
+def is_four_of_a_kind( hand )
+
+end
+
+def is_straight_flush( hand )
+
+end
+
+def is_royal_flush( hand )
+
+end
+
+
 def play_hand( hand )
+	score = []
+	high_car = is_high_card( hand )
+	one_pair = is_one_pair( hand )
+	two_pair = is_two_pair( hand )
+	three_of_a_kind = is_three_of_a_kind( hand )
+	straight = is_straight( hand )
+	flush = is_flush( hand )
+	full_house = is_full_house( hand )
+	four_of_a_kind = is_four_of_a_kind( hand )
+	straight_flush = is_straight_flush( hand )
+	royal_flush = is_royal_flush( hand )
+
 
 end
 
@@ -98,4 +187,11 @@ def poker_hands( filename )
 	return game
 end
 
-print poker_hands( "problem_54.txt" ),"\n"
+#print poker_hands( "problem_54.txt" ),"\n"
+#print is_one_pair( [ "5H", "5H", "6S", "6S", "7S" ] ), "\n"
+#print is_two_pair( [ "5H", "5H", "4S", "6S", "7S" ] ), "\n"
+#print is_three_of_a_kind( [ "5H", "5H", "6S", "6S", "9S" ] ), "\n"
+#print card_value( "QH" ), "\n"
+#print is_straight( [ "1H", "2H", "8S", "4S", "7S" ] ), "\n"
+#print is_flush( [ "1H", "2H", "8H", "4H", "7Q" ] ), "\n"
+print is_full_house( [ "1H", "1H", "1H", "4H", "4Q" ] ), "\n"

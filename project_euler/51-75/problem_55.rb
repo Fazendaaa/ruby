94cac5ca8907a96ae49081f5a17de55cf900a718
Farm <lucas.carotta@outlook.com>
2017-02-09 16:41:13 -0200

    [NOT SOLVED] Just adding it.

--- project_euler/51-75/problem_55.rb
+++ project_euler/51-75/problem_55.rb
@@ -5,9 +5,9 @@
 
 	Not all numbers produce palindromes so quickly. For example,
 
-						349 + 943 = 1292,
-						1292 + 2921 = 4213
-						4213 + 3124 = 7337
+							349 + 943 = 1292,
+							1292 + 2921 = 4213
+							4213 + 3124 = 7337
 
 	That is, 349 took three iterations to arrive at a palindrome.
 
@@ -16,11 +16,14 @@
 	through  the  reverse and add process is called a Lychrel number. Due to the
 	theoretical nature of these numbers, and for the purpose of this problem, we
 	shall  assume  that  a number is Lychrel until proven otherwise. In addition
-	you  are  given that for every number below ten-thousand, it will either (i)
-	become a palindrome in less than fifty iterations, or, (ii) no one, with all
-	the  computing  power  that  exists,  has  managed  so  far  to  map it to a
-	palindrome.  In  fact, 10677 is the first number to be shown to require over
-	fifty iterations before producing a palindrome: 4668731596684224866951378664
+	you are given that for every number below ten-thousand, it will either:
+
+		I. Become a palindrome in less than fifty iterations
+		II. No one, with all the computing power that exists, has managed so far
+			to map it to a palindrome. 
+
+	In  fact, 10677  is  the  first  number  to  be  shown to require over fifty
+	iterations   before  producing  a  palindrome:  4668731596684224866951378664
 	(53 iterations, 28-digits).
 
 	Surprisingly,  there  are  palindromic  numbers  that are themselves Lychrel
@@ -31,3 +34,8 @@
 	NOTE:  Wording  was  modified  slightly  on 24  April  2007 to emphasise the
 	theoretical nature of Lychrel numbers.	
 =end
+
+def lychrel_numbers( limit )
+end
+
+puts lychrel_numbers( 10_000 )

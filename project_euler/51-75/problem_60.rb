875c485247d3340e96a1e7ec7fcebc39cc84c8bd
Farm <lucas.carotta@outlook.com>
2017-02-11 11:04:23 -0200

    [NOT SOLVED] The logic is 'right', but to slow. Need to improve.

--- project_euler/51-75/problem_60.rb
+++ project_euler/51-75/problem_60.rb
@@ -9,4 +9,50 @@
 
 	Find  the  lowest  sum  for  a  set  of five primes for which any two primes
 	concatenate to produce another prime.
+
+	Helped me out:
+		*	http://www.mathplanet.com/education/algebra-2/discrete-mathematics-and-probability/permutations-and-combinations
 =end
+
+require_relative '../project_euler'
+
+def prime_pair_sets( min )
+	hash = Hash.new( false )
+	primes = []
+	matches = 0
+	prime = 0
+	# => P ( n , r ) = n ! ( n − r ) !
+	n_permutation = ( 1..min ).reduce( :* ) / ( 1..min-2 ).reduce( :* )
+	pair = nil
+
+	while matches < n_permutation do
+		prime = next_prime( prime )
+		primes.push( prime )
+
+		for a in primes.permutation( min ) do
+			matches = 0
+
+			for b in a.permutation( 2 ) do
+				new_number = b.join.to_i
+		
+				if hash[ new_number ] || is_prime( new_number ) then
+					hash[ new_number ] = new_number
+					matches += 1
+				# => all numbers must be primes
+				else
+					break
+				end
+			end
+
+
+			if matches == n_permutation then
+				pair = a
+				break
+			end
+		end
+	end
+
+	return pair
+end
+
+puts prime_pair_sets( 4 )

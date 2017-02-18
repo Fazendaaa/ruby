9bdba61428246e89fff238483dd1cf32d14b4d06
Farm <lucas.carotta@outlook.com>
2017-02-18 11:18:08 -0200

    Just adding, still not done.

---
+++ just_to_fun/damm_algorithm.rb
@@ -0,0 +1,10 @@
+=begin
+							Damm algorithm
+
+	In  error  detection,  the  Damm  algorithm  is a check digit algorithm that
+	detects  all  single-digit  errors and all adjacent transposition errors. It
+	was presented by H. Michael Damm in 2004.
+
+	Helped me out:
+		*	https://en.wikipedia.org/wiki/Damm_algorithm
+=end
---
+++ just_to_fun/verhoeff_algorithm.rb
@@ -0,0 +1,12 @@
+=begin
+							Verhoeff algorithm
+
+	The  Verhoeff  algorithm is a checksum formula for error detection developed
+	by the Dutch mathematician Jacobus Verhoeff and was first published in 1969.
+	It   was   the  first  decimal  check  digit  algorithm  which  detects  all
+	single-digit  errors,  and  all  transposition errors involving two adjacent
+	digits, which was at the time thought impossible with such a code.	
+
+	Helped me out:
+		*	https://en.wikipedia.org/wiki/Verhoeff_algorithm
+=end

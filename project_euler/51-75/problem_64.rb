ad07ef9f76dc2b612a4b99931bfda47c47116a85
Farm <lucas.carotta@outlook.com>
2017-02-21 12:40:09 -0300

    [NOT SOLVED] Just starting.

--- project_euler/51-75/problem_64.rb
+++ project_euler/51-75/problem_64.rb
@@ -13,18 +13,46 @@
 	The first ten continued fraction representations of (irrational) square
 	roots are:
 
-	√2=[1;(2)], period=1
-	√3=[1;(1,2)], period=2
-	√5=[2;(4)], period=1
-	√6=[2;(2,4)], period=2
-	√7=[2;(1,1,1,4)], period=4
-	√8=[2;(1,4)], period=2
-	√10=[3;(6)], period=1
-	√11=[3;(3,6)], period=2
-	√12= [3;(2,6)], period=2
-	√13=[3;(1,1,1,1,6)], period=5
+					√2=[1;(2)], period=1
+					√3=[1;(1,2)], period=2
+					√5=[2;(4)], period=1
+					√6=[2;(2,4)], period=2
+					√7=[2;(1,1,1,4)], period=4
+					√8=[2;(1,4)], period=2
+					√10=[3;(6)], period=1
+					√11=[3;(3,6)], period=2
+					√12= [3;(2,6)], period=2
+					√13=[3;(1,1,1,1,6)], period=5
 
 	Exactly four continued fractions, for N ≤ 13, have an odd period.
 
 	How many continued fractions for N ≤ 10000 have an odd period?
+
+	Helped me out:
+		*	https://youtu.be/R5HhNmFPLPQ
+		*	https://youtu.be/CaasbfdJdJg
+		*	http://stackoverflow.com/a/18163626/7092954
 =end
+
+require_relative '../project_euler'
+
+def continued_fractions( number )
+	reamain = Math.sqrt( number )
+	period = []
+
+
+
+	return period
+end
+
+def odd_period_square_roots( limit )
+	sqrt_roots = Array.new( limit -1 )
+
+	for i in 2..limit do
+		sqrt_roots.push( continued_fractions( i ) )
+	end
+
+	return sqrt_roots.select { | e | ( e.length-1 ).is_odd? }
+end
+
+puts odd_period_square_roots( 13 ).length
